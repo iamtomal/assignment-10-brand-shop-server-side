a179dac8eecd0cdf296308e56063260a7d692ef3
iamtomal <developer.tomal@gmail.com>
2023-10-19 05:39:03 +0600

    get data for showing all  product in client server

--- index.js
+++ index.js
@@ -31,7 +31,11 @@ async function run() {
     const productCollection = client.db('productDB').collection('product');
 
 
- 
+    app.get('/product', async(req,res)=>{
+        const cursor = productCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
 
     app.post('/product', async(req,res)=>{
         const newProduct = req.body;
